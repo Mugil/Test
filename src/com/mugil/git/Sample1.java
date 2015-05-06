a5a566591d2c2ae1e783fcbd958e95d31e1fae5f
Mugil <mugil.cse@gmail.com>
2015-05-05 22:36:59 -0700

    Initial Commit

---
+++ src/com/mugil/git/Sample1.java
@@ -0,0 +1,13 @@
+package com.mugil.git;
+
+public class Sample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample GIT Commit");
+	}
+
+}
